4424f9d43e5acabb73e4a5d9ac176e059f23ba7b
Dmig <Dmig@dimann>
2018-01-15 17:08:17 +0300

    Leader pawn drawing start

--- src/ru/dmig/pawns/gui/Panel.java
+++ src/ru/dmig/pawns/gui/Panel.java
@@ -42,70 +42,71 @@ public class Panel extends JPanel {
     public static final Color MY_ORANGE = new Color(219, 118, 67);
     public static final Color MY_WHITE = new Color(255, 255, 255);
     public static final Color MY_BLUE = new Color(25, 42, 90);
-    
+
     private static final boolean DRAW_HUNDRED = false;
+    private static final boolean LEADER_DRAWING = true;
 
     @Override
     public void paint(Graphics g) {
-        Graphics2D gr2d = (Graphics2D) g;
-        gr2d.setBackground(MY_WHITE);
-        gr2d.setColor(Color.PINK);
+        Graphics2D gr = (Graphics2D) g;
+        gr.setBackground(MY_WHITE);
+        
+        if (DRAW_HUNDRED) {
+            gr.setColor(Color.GRAY);
+            gr.drawLine(100, 100, 200, 100);
+        }
+        
+        gr.setColor(Color.PINK);
+        drawDangerZone(gr);
+        
+        drawAllFood(gr);
+        drawAllPawns(gr);
 
-        drawDangerZone(gr2d);
-
-        gr2d.setColor(MY_ORANGE);
-        for (int i = 0; i < Game.pawns.length; i++) {
-            if (Game.pawns[i].isAlive()) {
-                if (i == 0) {
-                    gr2d.setColor(Color.blue);
-                    drawPawn(gr2d, Game.pawns[i]);
-                    gr2d.setColor(MY_ORANGE);
-                } else {
-                    drawPawn(gr2d, Game.pawns[i]);
-                }
-            }
+        if (Game.KILLER_ENABLED) {
+            drawAllKillers(gr);
         }
 
-        gr2d.setColor(Color.red);
+        gr.setColor(Color.red);
 
         Game.bullets.forEach((bullet) -> {
             int x = Math.round(bullet.getX());
             int y = Math.round(bullet.getY());
-            gr2d.drawOval(x - BULLET_DIAMETER / 2, y - BULLET_DIAMETER / 2, BULLET_DIAMETER, BULLET_DIAMETER);
-            gr2d.drawLine(x, y, (int) Math.round(x + Math.cos(bullet.getAbsAngle()) * BULLET_DIAMETER),
+            gr.drawOval(x - BULLET_DIAMETER / 2, y - BULLET_DIAMETER / 2, BULLET_DIAMETER, BULLET_DIAMETER);
+            gr.drawLine(x, y, (int) Math.round(x + Math.cos(bullet.getAbsAngle()) * BULLET_DIAMETER),
                     (int) Math.round(y + Math.sin(bullet.getAbsAngle()) * BULLET_DIAMETER));
         });
 
-        gr2d.setColor(MY_BLUE);
-        synchronized (Game.foods) {
 
+    }
+
+    private void drawAllKillers(Graphics2D gr) {
+        gr.setColor(Color.RED);
+
+        for (int i = 0; i < Game.killers.size(); i++) {
+            int x = Math.round(Game.killers.get(i).getX());
+            int y = Math.round(Game.killers.get(i).getY());
+            gr.drawOval(x - KILLER_DIAMETER / 2, y - KILLER_DIAMETER / 2, KILLER_DIAMETER, KILLER_DIAMETER);
+        }
+    }
+
+    private void drawAllFood(Graphics2D g) {
+        g.setColor(MY_BLUE);
+        synchronized (Game.foods) {
             for (Iterator<Agent> itr = Game.foods.iterator(); itr.hasNext();) {
                 try {
-                    Agent food = itr.next();
-                    int x = Math.round(food.getX());
-                    int y = Math.round(food.getY());
-                    gr2d.drawOval(x - FOOD_DIAMETER / 2, y - FOOD_DIAMETER / 2, FOOD_DIAMETER, FOOD_DIAMETER);
+                    drawFood(g, itr.next());
                 } catch (ConcurrentModificationException ex) {
                     System.err.println("ConcurrentEx: " + ex);
                 }
             }
-
         }
 
-        if (Game.KILLER_ENABLED) {
-            gr2d.setColor(Color.RED);
+    }
 
-            for (int i = 0; i < Game.killers.size(); i++) {
-                int x = Math.round(Game.killers.get(i).getX());
-                int y = Math.round(Game.killers.get(i).getY());
-                gr2d.drawOval(x - KILLER_DIAMETER / 2, y - KILLER_DIAMETER / 2, KILLER_DIAMETER, KILLER_DIAMETER);
-            }
-        }
-
-        if(DRAW_HUNDRED) {
-            gr2d.setColor(Color.GRAY);
-            gr2d.drawLine(100, 100, 200, 100);
-        }
+    private void drawFood(Graphics2D g, Agent food) {
+        final int x = Math.round(food.getX());
+        final int y = Math.round(food.getY());
+        g.drawOval(x - FOOD_DIAMETER / 2, y - FOOD_DIAMETER / 2, FOOD_DIAMETER, FOOD_DIAMETER);
     }
 
     private void drawDangerZone(Graphics2D g) {
@@ -116,6 +117,32 @@ public class Panel extends JPanel {
         g.fillRect(len - dng, dng, dng, heg - dng);
         g.fillRect(0, heg - dng, len - dng, dng);
         g.fillRect(0, dng, dng, heg - dng - dng);
+    }
+
+    private void drawAllPawns(Graphics2D g) {
+        g.setColor(MY_ORANGE);
+
+        if (LEADER_DRAWING) {
+            boolean leaderDrawed = false;
+            for (Pawn pawn : Game.pawns) {
+                if (pawn.isAlive()) {
+                    if (!leaderDrawed) {
+                        g.setColor(Color.blue);
+                        drawPawn(g, pawn);
+                        g.setColor(MY_ORANGE);
+                        leaderDrawed = true;
+                    } else {
+                        drawPawn(g, pawn);
+                    }
+                }
+            }
+        } else {
+            for (Pawn pawn : Game.pawns) {
+                if (pawn.isAlive()) {
+                    drawPawn(g, pawn);
+                }
+            }
+        }
     }
 
     private void drawPawn(Graphics2D g, Pawn pawn) {
